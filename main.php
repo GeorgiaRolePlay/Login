68ea6365dee972852e3a94b7fd95dd59388912e8
GeorgiaRolePlay <145910022+GeorgiaRolePlay@users.noreply.github.com>
2023-10-02 23:48:39 +0400

    Add files via upload

---
+++ main.php
@@ -0,0 +1,21 @@
+<?php 
+$ip_address = $_SERVER['REMOTE_ADDR'];
+$ip_blocked = array('127.0.0.1','100.100.100.100');
+
+foreach($ip_blocked as $ip){
+    if($ip==$ip_address){
+        die();
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $ip_address ?>
+</body>
+</html>
